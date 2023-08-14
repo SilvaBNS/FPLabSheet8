0db131fd3a0ff8d274872902b4184bb1db360d06
SilvaBNS <136633217+SilvaBNS@users.noreply.github.com>
2023-08-14 21:34:07 +0530

    Update Q3.scala

--- Q3.scala
+++ Q3.scala
@@ -1,26 +1,39 @@
-object Q3 {
-  def main(args : Array[String]): Unit = {
-    val inputString = scala.io.StdIn.readLine("Enter the string : ")
+object Q_3 {
+  def main(args: Array[String]): Unit = {
+    val inputName = scala.io.StdIn.readLine("Enter the name that you want to format - ")
+
     println("Enter the number of the formatting type from the list : ")
     println("   1 - Format to uppercase")
     println("   2 - Format only the second letter to uppercase")
     println("   3 - Format to lowercase")
     println("   4 - Format only the last letter to uppercase")
-    val choice = scala.io.StdIn.readLine("Enter the choice : ").toInt
+    println()
 
-    val outputStr : (String, Int) => String = (inputString : String, choice : Int )=> {
-      choice match {
-        case choice if (choice == 1) => inputString.toUpperCase()
-        case choice if (choice == 2) => inputString.head.toString + inputString.charAt(1).toString.toUpperCase() + inputString.drop(2).toString
-        case choice if (choice == 3) => inputString.toLowerCase()
-        case choice if (choice == 4) => inputString.init.toString+inputString.last.toString.toUpperCase()
-      }
+    val choice = scala.io.StdIn.readLine("Enter your choice - ").toInt
 
-    }
-    val result = outputStr(inputString, choice)
-    println(result)
+    val formattedName = selectChoice(choice, inputName)
+    println("Formatted based on the provided formatFunction - " + formattedName)
   }
 
+  val toUpper: String => String = (inputString: String) => {
+    inputString.toUpperCase
+  }
 
+  val toLower: String => String = (inputString: String) => {
+    inputString.toLowerCase
+  }
+
+  val formatNames: (String, String => String) => String = (name, formatFunction) => {
+    formatFunction(name)
+  }
+
+  val selectChoice: (Int, String) => String = (choice, inputName) => {
+    choice match {
+      case 1 => formatNames(inputName, toUpper)
+      case 2 => formatNames(inputName, str => str.head + (str.charAt(1).toUpper + str.drop(2)))
+      case 3 => formatNames(inputName, toLower)
+      case 4 => formatNames(inputName, str => str.init + str.last.toUpper)
+      case _ => "You made an Invalid choice"
+    }
+  }
 }
-
